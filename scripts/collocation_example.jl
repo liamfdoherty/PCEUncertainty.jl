cecd4e965f50f9572b4bd939460c4d272ab6cc81
liamfdoherty <lfd27@icloud.com>
2022-01-06 18:58:11 -0500

    fixed collocation points to match paper

--- scripts/collocation_example.jl
+++ scripts/collocation_example.jl
@@ -2,8 +2,7 @@ using Distributions
 using Plots
 using PCEUncertainty
 
-N = 5
-dist = Uniform(-2, 2)
+N = 4
 
 # Plot the true solution
 x_vals = LinRange(-3, 3, 100)
@@ -11,14 +10,10 @@ truth(x) = exp(-x)
 plt = plot(x_vals, truth.(x_vals), title = "Collocation points vs. Truth", label = "Truth")
 
 # Compute and plot the collocation points
-
-# p_vals, sols = generate_collocation_points(N, dist) # for if we want to sample collocation points from dist
-p_vals, sols = generate_collocation_points(N) # x values are Gauss-Legendre quadrature points
+nodes, weights, sols = generate_collocation_points(N) # x values are Gauss-Hermite quadrature points
 end_states = [sols[i].u[end] for i in 1:N]
-scatter!(p_vals, end_states, label = "Collocation Points")
+scatter!(nodes, end_states, label = "Collocation Points")
 xlabel!("z"); ylabel!("u(t = 1; z)")
-
-# Compute and plot the interpolated solution
 
 
 
--- src/generate_collocation.jl
+++ src/generate_collocation.jl
@@ -1,8 +1,10 @@
 using PCEUncertainty
 
-function generate_collocation_points(N::Int, )
+function generate_collocation_points(N::Int)
     # Get the nodes and weights for the quadrature rule
-    p_vals = sqrt(2)*gausshermite(N)[1]
+    ortho_polynomials = GaussOrthoPoly(N)
+    nodes = ortho_polynomials.quad.nodes
+    weights = ortho_polynomials.quad.weights
     sols = []
 
     # Solve the ODE numerically at the collocation points over the probability space
@@ -11,12 +13,12 @@ function generate_collocation_points(N::Int, )
         f(u, p, t) = -p[1]*u
         u0 = 1.
         tspan = (0., 1.)
-        p = p_vals[i]
+        p = nodes[i]
 
         # Solve the ODE
         prob = ODEProblem(f, u0, tspan, [p])
         sol = solve(prob)
         push!(sols, sol)
     end
-    return p_vals, sols
+    return nodes, weights, sols
 end
